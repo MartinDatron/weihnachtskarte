e2a78a577bc432e04a8788762d56e795f321580f
MartinDatron <martin.doerr@datron.de>
2022-12-23 12:36:31 +0100

    Language Support

--- name.js
+++ name.js
@@ -1,14 +1,27 @@
-const name = getNameFromUrl() || "ihr Lieben"
+const name = getNameFromUrl("name") || "ihr Lieben"
+const language = getNameFromUrl("lang") || "Frohe Weihnachten, "
 
+const mainEle = document.getElementById("lang");
 const nameEle = document.getElementById("name");
 
 createNameSpans(name);
+createMainText(language);
 
 const letterEles = document.querySelectorAll(".letter");
 
 console.log(letterEles)
 
 lightUpName(1);
+
+function createMainText(language){
+    let htmlStringMain = "";
+    if (language == "DE") {
+        htmlStringMain = `<span class="mainText">Frohe Weihnachten, </span>`
+    } else if (language == "EN") {
+        htmlStringMain = `<span class="mainText">Merry Christmas, </span>`
+    }
+    mainEle.innerHTML = htmlStringMain;
+}
 
 function createNameSpans(name) {
     let namenArray = name.split("");
@@ -32,9 +45,9 @@ function lightUpName(n){
     }, 500)
 }
 
-function getNameFromUrl() {
+function getNameFromUrl(queryName) {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    let urlName = urlParams.get("name");
+    let urlName = urlParams.get(queryName);
     return urlName;
 }
